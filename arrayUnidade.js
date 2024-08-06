b5d0071ccbd105ee9ac4474a65fcf71ca80c30ae
Droka <112392282+Pedrolil460@users.noreply.github.com>
2024-08-06 09:10:47 -0300

    06/08/24

---
+++ arrayUnidade.js
@@ -0,0 +1,7 @@
+var unidades= ["Segurança da Informação”, “Game Designe”, “Ciência da computação"];
+
+function unidadeEscolhida(numero){
+return unidades[numero];
+}
+
+console.log(unidadeEscolhida(0));
